050e1936c958760c0a8833d7c0a8410640e3b404
Rohitjadhav19899 <122199406+Rohitjadhav19899@users.noreply.github.com>
2023-11-17 22:34:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let saveEl1 = document.getElementById("save-el1")
+let countEl1 = document.getElementById("count-el1")
+let count1 = 0
 
-function increment() {
-    count += 1
-    countEl.textContent = count
+let saveEl2 = document.getElementById("save-el2")
+let countEl2 = document.getElementById("count-el2")
+let count2 = 0
+
+function increment1() {
+    count1 += 1
+    countEl1.textContent = count1
 }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+function save1() {
+    let countStr = count1 + " - "
+    saveEl1.textContent += countStr
+    countEl1.textContent = 0
+    count1 = 0
+}
+
+function increment2() {
+    count2 += 1
+    countEl2.textContent = count2
+}
+
+function save2() {
+    let countStr = count2 + " - "
+    saveEl2.textContent += countStr
+    countEl2.textContent = 0
+    count2 = 0
 }
